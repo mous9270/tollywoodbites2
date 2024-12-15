ceaca06a34d7b17b30a99c7f49049e1e089a816f
mous9270 <mous9270222@gmail.com>
2024-12-15 18:15:48 +0530

    add rectangle behing the order now image

--- src/components/OrderButton.jsx
+++ src/components/OrderButton.jsx
@@ -1,11 +1,14 @@
-// components/OrderButton.jsx
 import React from 'react';
 
 const OrderButton = () => (
-  <div className="absolute bottom-8 right-8 w-32 h-32 bg-[#FBA40E] flex flex-col items-center justify-center text-black cursor-pointer hover:bg-[#FBA40E]/90 transition-colors">
-    <div>
+  <div className="absolute bottom-8 right-8 w-32 h-32">
+    {/* Rotating border */}
+    <div className="absolute inset-0 border border-[#FBA40E] animate-spin [animation-duration:12s]" />
+    
+    {/* Main button */}
+    <div className="absolute inset-0 bg-[#FBA40E] flex flex-col items-center justify-center text-black cursor-pointer hover:bg-[#FBA40E]/90 transition-colors">
       <img src="/hero-icon.png" alt="Order" className="w-12 h-12 mb-2 mx-auto" />
-      <span className="text-xs font-bold tracking-wider">ORDER NOW</span>
+      <span className="text-xs font-bold tracking-wider uppercase">Order Now</span>
     </div>
   </div>
 );
